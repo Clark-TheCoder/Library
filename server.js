9d1b45e812d1da9a80ad3d00e10b6ed28789784b
Emma Clark <emmabossuytclark@gmail.com>
2025-09-03 13:41:12 -0300

    Added routers to server

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ dotenv.config();
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import { createUserTable } from "./models/users.js";
+import { createBooksTable } from "./models/books.js";
+import authRoutes from "./routes/auth.js";
+import bookRoutes from "./routes/books.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Create tables
+await createUserTable();
+await createBooksTable();
 
 // Serve static files
 const __filename = fileURLToPath(import.meta.url);
@@ -14,6 +23,13 @@ app.use(express.static(join(__dirname, "public")));
 
 // Test route
 app.get("/", (req, res) => res.send("Server running with MySQL!"));
+
+// Auth routes
+app.use("/auth", authRoutes);
+// Book routes
+app.use("/books", bookRoutes);
+// User routes
+app.use("/users", userRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
