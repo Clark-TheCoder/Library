56cef74fcfb342153d7f8e41e7f6d0b7b41a304b
Emma Clark <emmabossuytclark@gmail.com>
2025-09-03 18:57:57 -0300

    Add POST /signup route using signupUser controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { signupUser } from "../controllers/auth/authController.js";
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
+
+router.post("/signup", signupUser);
 
 router.get("/login", (req, res) => {
   res.send("Login Page");
