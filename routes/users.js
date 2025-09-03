fd0e1c677c426f442338972999ce14e6d50d48bf
Emma Clark <emmabossuytclark@gmail.com>
2025-09-03 19:24:12 -0300

    Added route to dashboard.ejs

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 import express from "express";
 const router = express.Router();
 
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
 router.get("/library", (req, res) => {
   res.send("User library");
 });
