079755e33060bc1c800816e813d7f9214b29e5a1
Emma Clark <emmabossuytclark@gmail.com>
2025-09-03 19:40:44 -0300

    Added login route to render the login.ejs webpage

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
 router.post("/signup", signupUser);
 
 router.get("/login", (req, res) => {
