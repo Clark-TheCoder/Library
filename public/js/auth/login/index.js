31d70120b166ca3cfbe4985161df648b8f309382
Emma Clark <emmabossuytclark@gmail.com>
2025-09-04 14:34:45 -0300

    Added set up for the login webpage and the fetch for logins

---
+++ public/js/auth/login/index.js
@@ -0,0 +1,6 @@
+import { login } from "./login.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login_form");
+  loginForm?.addEventListener("submit", login);
+});
---
+++ public/js/auth/login/login.js
@@ -0,0 +1,25 @@
+export async function login(e) {
+  e.preventDefault();
+
+  // Get the form field values
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const data = { username, password };
+  console.log(data);
+  try {
+    const response = await fetch("/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    if (response.ok) {
+      //window.location.href = "/users/dashboard";
+    } else {
+      console.log(result.message);
+    }
+  } catch (error) {
+    return error;
+  }
+}
