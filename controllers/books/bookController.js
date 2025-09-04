3ad3018c92f6912ce31a3a1f9a552794c6a26db8
Emma Clark <emmabossuytclark@gmail.com>
2025-09-04 19:35:37 -0300

    Added controller to add a new book

--- controllers/books/bookController.js
+++ controllers/books/bookController.js
@@ -1,20 +1,29 @@
+import { createNewBook } from "../../models/books.js";
+
 export async function addBook(req, res) {
-  const { title, author, genre, rating, thoughts } = req.body;
-  if (!title || !author) {
+  if (!req.body.title || !req.body.author) {
     return res
       .status(400)
       .json({ message: "Please enter all required fields" });
   }
 
+  const bookData = {
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre || null,
+    rating: req.body.rating || null,
+    thoughts: req.body.thoughts || "",
+  };
+
   try {
-    //const result = await createNewBook();
-    // if (result) {
-    //   return res.status(400).json({ message: "Entry saved to your library" });
-    // } else {
-    //   return res
-    //     .status(400)
-    //     .json({ message: "Could not save your entry at this time" });
-    // }
+    const result = await createNewBook(bookData);
+    if (result) {
+      return res.status(400).json({ message: "Entry saved to your library" });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Could not save your entry at this time" });
+    }
   } catch (error) {
     return res.status(500).json({ message: "Server error" });
   }
