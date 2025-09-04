62cd99d284db47a57e218eae5a136cd61349b235
Emma Clark <emmabossuytclark@gmail.com>
2025-09-04 14:35:16 -0300

    Removed extra get /login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,17 @@
 import express from "express";
-import { signupUser } from "../controllers/auth/authController.js";
+import { signupUser, loginUser } from "../controllers/auth/authController.js";
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+router.post("/signup", signupUser);
+
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.post("/signup", signupUser);
-
-router.get("/login", (req, res) => {
-  res.send("Login Page");
-});
+router.post("/login", loginUser);
 
 export default router;
