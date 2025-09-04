b2027233472fccdddf63618a2a3818fafcd1944a
Emma Clark <emmabossuytclark@gmail.com>
2025-09-04 15:19:30 -0300

    Added route to render to createBook webpage

--- routes/books.js
+++ routes/books.js
@@ -2,11 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/createBook", (req, res) => {
-  res.send("Create Book");
-});
-
-router.get("/createBook", (req, res) => {
-  res.send("Create Book");
+  res.render("createBook");
 });
 
 export default router;
