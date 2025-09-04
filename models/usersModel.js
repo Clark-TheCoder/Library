496279a4c0645d4598f2e9a262b30fd6ed673d92
Emma Clark <emmabossuytclark@gmail.com>
2025-09-04 14:33:25 -0300

    Added authenication of user function

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,4 +1,5 @@
 import db from "./db.js";
+import bcrypt from "bcrypt";
 
 export async function createUserTable() {
   await db.query(`CREATE TABLE IF NOT EXISTS users (
@@ -12,13 +13,13 @@ export async function createUserTable() {
 }
 
 export async function getUserByUsername(username) {
-  const sql = "SELECT * FROM users WHERE username = ?";
+  const sql = "SELECT id, username, email FROM users WHERE username = ?";
   const [rows] = await db.execute(sql, [username]);
   return rows[0];
 }
 
 export async function getUserByEmail(email) {
-  const sql = "SELECT * FROM users WHERE email = ?";
+  const sql = "SELECT id, username, email FROM users WHERE email = ?";
   const [rows] = await db.execute(sql, [email]);
   return rows[0];
 }
@@ -27,4 +28,20 @@ export async function createUser(username, email, hashedPassword) {
   const sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
   const [row] = await db.execute(sql, [username, email, hashedPassword]);
   return row;
+}
+
+export async function authenticateUser(username, password) {
+  const sql = "SELECT * FROM users WHERE username = ?";
+  const [row] = await db.execute(sql, [username]);
+  const user = row[0];
+
+  if (!user) return false;
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return false;
+
+  const userWithoutPassword = { ...user };
+  delete userWithoutPassword.password;
+
+  return userWithoutPassword;
 }
