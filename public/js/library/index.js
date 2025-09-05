3cc63fa7f0e4e3f69a9abf4489f1180e8f55f681
Emma Clark <emmabossuytclark@gmail.com>
2025-09-05 20:32:16 -0300

    Added event listener to each bookEl

--- public/js/library/index.js
+++ public/js/library/index.js
@@ -1,12 +1,16 @@
 import { createBookUI } from "./createBookUI.js";
 import { loadBooksIntoLibrary } from "./getLibraryBooks.js";
+import { selectBookFromLibrary } from "./selectBook.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const books = await loadBooksIntoLibrary();
+  console.log(books);
 
   books.forEach((book) => {
     const shelf = document.getElementById("shelf");
     let bookEl = createBookUI(book);
     shelf.appendChild(bookEl);
+
+    bookEl.addEventListener("click", selectBookFromLibrary);
   });
 });
