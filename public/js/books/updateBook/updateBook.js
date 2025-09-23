f34c9c6d7f73d0bfa63154f924dc82fdff7771aa
Emma Clark <emmabossuytclark@gmail.com>
2025-09-23 18:57:11 -0300

    Completed fetch to update the book

--- public/js/books/updateBook/updateBook.js
+++ public/js/books/updateBook/updateBook.js
@@ -1,11 +1,31 @@
-export async function updateBook(bookId, formData) {
+export async function updateBook(bookId) {
+  const formData = {
+    title: title.value,
+    author: author.value,
+    genre: genre.value || "",
+    rating: rating.value || "",
+    thoughts: thoughts.value || "",
+  };
+
+  if (!formData.title) {
+    return false;
+  }
+  if (!formData.author) {
+    return false;
+  }
+
   try {
-    const response = await fetch(`/updateBook/${bookId}`, {
+    const response = await fetch(`/books/updateBook/${bookId}`, {
       method: "PATCH",
       credentials: "include",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     });
+
+    if (!response.ok) {
+      return false;
+    }
+    return true;
   } catch (error) {
     console.log(error);
   }
