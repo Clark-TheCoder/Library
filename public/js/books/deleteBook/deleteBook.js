1079dbf2b3c6db16b91df8fc5a758110fe2d9ed3
Emma Clark <emmabossuytclark@gmail.com>
2025-09-23 19:01:16 -0300

    Changed the return values if the server could not be reached or the book could not be deleted

--- public/js/books/deleteBook/deleteBook.js
+++ public/js/books/deleteBook/deleteBook.js
@@ -4,18 +4,15 @@ export async function deleteBook(bookId) {
       method: "DELETE",
       credentials: "include",
     });
-
     const data = await response.json();
-
     if (!response.ok) {
       console.error("Delete failed:", data.message);
-      return { success: false, message: data.message };
+      false;
     }
-
     console.log("Delete success:", data.message);
     return { success: true, message: data.message };
   } catch (error) {
     console.error("Network error:", error);
-    return { success: false, message: "Network error" };
+    return false;
   }
 }
