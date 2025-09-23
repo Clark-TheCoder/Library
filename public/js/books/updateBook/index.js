69eb7744cd0fc626cc74625b95bc1b112f09d750
Emma Clark <emmabossuytclark@gmail.com>
2025-09-23 16:33:52 -0300

    Added functionality to get the data from the selected book and display them in the updateBook form's value

--- public/js/books/updateBook/index.js
+++ public/js/books/updateBook/index.js
@@ -1,4 +1,5 @@
 import { getBookData } from "./loadBookData.js";
+import { updateBook } from "./updateBook.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const pathParts = window.location.pathname.split("/");
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     return;
   }
   // Get the form fields
+  const form = document.getElementById("update_book_form");
   const title = document.getElementById("title");
   const author = document.getElementById("author");
   const genre = document.getElementById("genre");
@@ -18,9 +20,33 @@ document.addEventListener("DOMContentLoaded", async () => {
   const thoughts = document.getElementById("thoughts");
 
   // Fill book data into the fields
-  title.value = book.bookData.title || "";
-  author.value = book.bookData.author || "";
+  title.value = book.bookData.title;
+  author.value = book.bookData.author;
   genre.value = book.bookData.genre || "";
   rating.value = book.bookData.rating || "";
   thoughts.value = book.bookData.thoughts || "";
+
+  // Sumbit form
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if (!title.value.trim()) {
+      alert("Title is required.");
+      return;
+    }
+    if (!author.value.trim()) {
+      alert("Author is required.");
+      return;
+    }
+
+    const formData = {
+      title: title.value,
+      author: author.value,
+      genre: genre.value || "",
+      rating: rating.value || "",
+      thoughts: thoughts.value || "",
+    };
+
+    await updateBook(bookId, formData);
+  });
 });
