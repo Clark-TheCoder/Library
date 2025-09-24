197bd63e4534b22f5e1c7b1c8efad3360a3db6e5
Emma Clark <emmabossuytclark@gmail.com>
2025-09-24 17:30:06 -0300

    Change catch to send user to the 404 error page

--- public/js/auth/login/index.js
+++ public/js/auth/login/index.js
@@ -2,5 +2,8 @@ import { login } from "./login.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("login_form");
-  loginForm?.addEventListener("submit", login);
+  loginForm?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await login();
+  });
 });
