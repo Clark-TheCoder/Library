998b8e0c615656b492389d3fff676dfa0cb12426
Emma Clark <emmabossuytclark@gmail.com>
2025-09-26 21:05:16 -0300

    Added function to render books from the getBestSellers fetch

--- public/js/dashboard/index.js
+++ public/js/dashboard/index.js
@@ -8,5 +8,36 @@ document.addEventListener("DOMContentLoaded", async () => {
   quote.innerHTML = text;
   quotedBy.innerHTML = author;
 
-  await getBestSellers();
+  const data = await getBestSellers();
+  console.log(data);
+  createBooks(data);
 });
+
+function createBooks(booksArray) {
+  const bestSellersContainer = document.getElementById(
+    "best_sellers_container"
+  );
+  booksArray.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book_div");
+
+    const img = document.createElement("img");
+    img.src = book.book_image;
+    const bookTextDiv = document.createElement("div");
+    bookTextDiv.classList.add("book_text");
+    const title = document.createElement("h5");
+    title.innerHTML = book.title;
+    const author = document.createElement("h6");
+    author.innerHTML = book.author;
+    const rank = document.createElement("h4");
+    rank.innerHTML = book.rank;
+    bookTextDiv.appendChild(title);
+    bookTextDiv.appendChild(author);
+    bookTextDiv.appendChild(rank);
+
+    bookDiv.appendChild(img);
+    bookDiv.appendChild(bookTextDiv);
+
+    bestSellersContainer.appendChild(bookDiv);
+  });
+}
