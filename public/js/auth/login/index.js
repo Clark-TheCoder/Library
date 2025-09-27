d002db1fcb6ec15adb2642798ea8434cac092175
Emma Clark <emmabossuytclark@gmail.com>
2025-09-27 14:11:59 -0300

    Removed the DOMContentLoaded event

--- public/js/auth/login/index.js
+++ public/js/auth/login/index.js
@@ -1,9 +1,7 @@
 import { login } from "./login.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const loginForm = document.getElementById("login_form");
-  loginForm?.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    await login();
-  });
+const loginForm = document.getElementById("login_form");
+loginForm?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  await login();
 });
