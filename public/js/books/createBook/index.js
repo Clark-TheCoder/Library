52e56c948e2101b84ccf8126ae69e9214a4eae29
Emma Clark <emmabossuytclark@gmail.com>
2025-09-27 14:16:34 -0300

    Remoded the DOMContentLoaded event

--- public/js/books/createBook/index.js
+++ public/js/books/createBook/index.js
@@ -1,6 +1,4 @@
 import { createBook } from "./createBook.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const createBookForm = document.getElementById("create_book_form");
-  createBookForm.addEventListener("submit", createBook);
-});
+const createBookForm = document.getElementById("create_book_form");
+createBookForm.addEventListener("submit", createBook);
