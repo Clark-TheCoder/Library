b235990aca1a6c0b323eba7bb06195b8c719af6c
Emma Clark <emmabossuytclark@gmail.com>
2025-09-28 14:56:49 -0300

    Added file and function to get the user's location which will be utilized in the getWeather function

---
+++ public/js/dashboard/api/getLocation.js
@@ -0,0 +1,19 @@
+export function getLocation() {
+  return new Promise((resolve, reject) => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve({
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+          });
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    } else {
+      reject(new Error("Geolocation not supported"));
+    }
+  });
+}
