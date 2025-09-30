a2189ce0acab44ecbfe79df4f4b586f0187e7415
Emma Clark <emmabossuytclark@gmail.com>
2025-09-30 07:43:40 -0300

    Moved functions that display data from index.js into the ui folder

--- public/js/dashboard/index.js
+++ public/js/dashboard/index.js
@@ -1,43 +1,19 @@
 import { getBestSellers } from "./api/getBestSellers.js";
 import { getQuote } from "./api/getQuote.js";
+import { getWeather, findReadingDay } from "./api/getWeather.js";
+import { createBooks } from "./ui/displayBestSellers.js";
+import { displayQuote } from "./ui/displayQuote.js";
+import { displayGoodReadingDay } from "./ui/displayWeather.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const quote = document.getElementById("quote");
-  const quotedBy = document.getElementById("quotedBy");
   let { text, author } = await getQuote();
-  quote.innerHTML = text;
-  quotedBy.innerHTML = author;
+  displayQuote(text, author);
 
   const data = await getBestSellers();
-  console.log(data);
   createBooks(data);
+
+  let forecast = await getWeather();
+  console.log(forecast);
+  let day = findReadingDay(forecast);
+  displayGoodReadingDay(day);
 });
-
-function createBooks(booksArray) {
-  const bestSellersContainer = document.getElementById(
-    "best_sellers_container"
-  );
-  booksArray.forEach((book) => {
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book_div");
-
-    const img = document.createElement("img");
-    img.src = book.book_image;
-    const bookTextDiv = document.createElement("div");
-    bookTextDiv.classList.add("book_text");
-    const title = document.createElement("h5");
-    title.innerHTML = book.title;
-    const author = document.createElement("h6");
-    author.innerHTML = book.author;
-    const rank = document.createElement("h4");
-    rank.innerHTML = book.rank;
-    bookTextDiv.appendChild(title);
-    bookTextDiv.appendChild(author);
-    bookTextDiv.appendChild(rank);
-
-    bookDiv.appendChild(img);
-    bookDiv.appendChild(bookTextDiv);
-
-    bestSellersContainer.appendChild(bookDiv);
-  });
-}
