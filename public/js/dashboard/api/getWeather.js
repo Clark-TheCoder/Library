269d2be8f74df374d5a2f5aa60e509ce9e911297
Emma Clark <emmabossuytclark@gmail.com>
2025-09-30 07:46:38 -0300

    Refined styling

--- public/js/dashboard/api/getWeather.js
+++ public/js/dashboard/api/getWeather.js
@@ -26,14 +26,69 @@ export async function getWeather() {
         maxTemp: day.tempmax,
         minTemp: day.tempmin,
         precipprob: day.precipprob,
-        snow: day.snow,
         uvindex: day.uvindex,
         windspeed: data.windspeed,
       }));
+      return simpleForecast;
     } else {
-      console.log(data.message);
+      return [];
     }
   } catch (error) {
-    console.log("error getting to backend");
+    return [];
   }
+}
+
+export function findReadingDay(weatherArray) {
+  for (let i = 0; i < weatherArray.length; i++) {
+    let day = weatherArray[i];
+    let reason = [];
+    let weatherCondition = [];
+    if (day.maxTemp >= 105) {
+      reason.push("Extreme heat warning");
+      weatherCondition.push("Hot");
+    }
+    if (day.minTemp <= 10 && day.precipprob < 40) {
+      reason.push("Extreme cold warning");
+      weatherCondition.push("Cold");
+    }
+    if (day.precipprob > 40 && day.maxTemp <= 32) {
+      reason.push("High chance of snow");
+      weatherCondition.push("Snow");
+    }
+    if (day.precipprob > 40 && day.maxTemp > 32) {
+      reason.push("High chance of rain");
+      weatherCondition.push("Rain");
+    }
+    if (day.uvindex > 9) {
+      reason.push("High UV index");
+      weatherCondition.push("Sunny");
+    }
+    if (day.windspeed >= 74) {
+      reason.push("High winds");
+      weatherCondition.push("Wind");
+    }
+    // If this day has reasons, return it
+    if (reason.length > 0) {
+      return {
+        index: i,
+        day,
+        reason,
+        weatherCondition,
+      };
+    }
+    if (reason.length > 0) {
+      return {
+        index: i,
+        day,
+        reason,
+        weatherCondition,
+      };
+    }
+  }
+  return {
+    index: -1,
+    day: null,
+    reason: null,
+    weatherCondition: null,
+  };
 }
